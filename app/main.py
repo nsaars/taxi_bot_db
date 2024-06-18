da2ae44e2a8bc9cb282c34439f239450332f5a64
nsaars <60110706+nsaars@users.noreply.github.com>
2024-06-18 17:46:36 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from settings import settings
 if __name__ == "__main__":
     granian.Granian(
         target="application:application",
-        address="0.0.0.0",  # noqa: S104
+        address="https://taxi_bot_db.up.railway.app",  # noqa: S104
         port=settings.app_port,
         interface=Interfaces.ASGI,
         log_dictconfig={"root": {"level": "INFO"}} if not settings.debug else {},
