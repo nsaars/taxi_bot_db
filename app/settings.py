357d905eff82745ed37e019c41ee3001b704f26c
nsaars <60110706+nsaars@users.noreply.github.com>
2024-06-18 21:17:04 +0500

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -19,7 +19,7 @@ class Settings(pydantic_settings.BaseSettings):
     db_echo: bool = False
     db_pool_pre_ping: bool = True
 
-    app_port: int = 26170
+    app_port: int = int(os.environ.get("PORT", settings.app_port))
 
     @property
     def db_dsn(self) -> URL:
